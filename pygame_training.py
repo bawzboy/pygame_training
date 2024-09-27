2717ffd9fda9b8d494b1c0eac129812b2e8aaa82
Helge <helgepeggen@web.de>
2024-09-27 12:33:51 +0200

    .

--- pygame_training.py
+++ pygame_training.py
@@ -3,13 +3,13 @@ from sys import exit
 
 
 pygame.init()
-screen = pygame.display.set_mode((400, 800))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
 # Player
 player_surf = pygame.image.load("graphics/player.png").convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (200, 780))
+player_rect = player_surf.get_rect(midbottom = (400, 600))
 
 
 while True:
@@ -17,13 +17,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_rect.x -= 10
+        if keys[pygame.K_RIGHT]:
+            player_rect.x += 10
+        if keys[pygame.K_UP]:
+            player_rect.y -= 10
+        if keys[pygame.K_DOWN]:
+            player_rect.y += 10
     
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player_rect.x -= 10
-            if event.key == pygame.K_RIGHT:
-                player_rect.x += 10
-        
 
     screen.fill("black")        
     screen.blit(player_surf, player_rect)
